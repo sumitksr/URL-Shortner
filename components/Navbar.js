d3d314cfbcd7179b5af76aff4bb4344f9008120e
sumitksr <sumitksr6223@gmail.com>
2025-07-30 09:43:03 +0530

    c

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <ul className="flex justify-between items-center text-white">
-        <Link href="/"><li className="text-xl font-bold">Url Shortner</li></Link>
+        <Link href="/"><li className="text-xl font-bold">BitZipp</li></Link>
         <div className="flex space-x-8">
           <Link href="/">
             <li className="hover:text-gray-300 cursor-pointer">Home</li>
