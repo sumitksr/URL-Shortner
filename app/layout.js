d7670e17970acec09dfe49b59fbcc0043212b90c
sumitksr <sumitksr6223@gmail.com>
2025-11-07 16:09:15 +0530

    feat: Add SpeedInsights component and update dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
